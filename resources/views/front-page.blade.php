c5b7eb886cd0e4d45f338b423852b72c5f8b522b
Anya L Keller <kelleranyal@gmail.com>
2020-09-02 17:14:14 -0400

    got redirect from home page working

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -12,13 +12,17 @@
     $main_video = new WP_Query($args);
   @endphp
 
-  @if($main_video->have_posts()) @php the_post() @endphp
+  @if($main_video->have_posts()) @php $main_video -> the_post() @endphp
+    @php
+      wp_safe_redirect( get_permalink() );
+      exit;
+    @endphp
+  @else
     @include('partials/main-video')
+    @while(have_posts()) @php the_post() @endphp
+      @include('partials.content-'.get_post_type())
+      @include('partials.content-page')
+    @endwhile
   @endif
 
-
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
-    @include('partials.content-page')
-  @endwhile
 @endsection
