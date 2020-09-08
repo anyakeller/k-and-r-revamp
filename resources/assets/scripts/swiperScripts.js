9839bd607b1fa5bec769508ecc4cef22b4b57f58
Anya L Keller <kelleranyal@gmail.com>
2020-09-08 14:07:53 -0400

    got video thumbnails in swiper, 2 rows, lazy loading too

--- resources/assets/scripts/swiperScripts.js
+++ resources/assets/scripts/swiperScripts.js
@@ -4,7 +4,15 @@ import Swiper from 'swiper/bundle';
 var mySwiper = new Swiper('.swiper-container', {
   slidesPerView: 3,
   spaceBetween: 5,
+  slidesPerColumn: 2,
   freeMode: true,
+  // Disable preloading of all images
+  preloadImages: false,
+  loadPrevNextAmount: 4,
+  watchSlidesVisibility: true,
+  // Enable lazy loading
+  lazy: true,
+  loadOnTransitionStart: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
@@ -17,17 +25,23 @@ var mySwiper = new Swiper('.swiper-container', {
     576: {
       slidesPerView: 3,
       spaceBetween: 20,
+      slidesPerColumn: 2,
+      loadPrevNextAmount: 5,
     },
     768: {
       slidesPerView: 3,
+      slidesPerColumn: 2,
       spaceBetween: 30,
     },
     992: {
       slidesPerView: 4,
+      slidesPerColumn: 2,
       spaceBetween: 20,
+      loadPrevNextAmount: 6,
     },
     1200: {
       slidesPerView: 4,
+      slidesPerColumn: 2,
       spaceBetween: 20,
     },
   },
--- resources/views/single-krvideo.blade.php
+++ resources/views/single-krvideo.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
+<style>
+  .swiper-container {
+    width: 100%;
+    height: calc((100px + 30px) * 2);
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+    height: 100px !important;
+  }
+</style>
+
 @section('content')
 {{-- main video player --}}
 @while(have_posts())
@@ -8,16 +23,24 @@
 @include('partials.content-single-'.get_post_type())
 @endwhile
 
+{{-- all the other videos --}}
+@php
+$all_videos = get_krvideos();
+@endphp
 {{-- swiper tests --}}
 <div class="swiper-container">
   <!-- Additional required wrapper -->
   <div class="swiper-wrapper">
-    <!-- Slides -->
-    <div class="swiper-slide">Slide 1</div>
-    <div class="swiper-slide">Slide 2</div>
-    <div class="swiper-slide">Slide 3</div>
-    <div class="swiper-slide">Slide 4</div>
-    <div class="swiper-slide">Slide 5</div>
+    @while($all_videos->have_posts())
+      @php $all_videos-> the_post()
+      @endphp
+      <div class="swiper-slide">
+        <img class="swiper-lazy img-fluid" data-src="{{ the_field('wistia_related_video_thumbnail')  }}">
+        <div class="swiper-lazy-preloader"></div>
+        <h5 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h5>
+      </div>
+      {{-- @include('partials.content-'.get_post_type()) --}}
+    @endwhile
   </div>
   <!-- If we need pagination -->
   <div class="swiper-pagination"></div>
@@ -26,16 +49,4 @@
   <div class="swiper-button-prev"></div>
   <div class="swiper-button-next"></div>
 </div>
-
-{{-- all the other videos --}}
-@php
-$all_videos = get_krvideos();
-@endphp
-@while($all_videos->have_posts())
-  @php $all_videos-> the_post()
-  @endphp
-  @include('partials.content-'.get_post_type())
-  @include('partials.content-page')
-  @endwhile
-
-  @endsection
+@endsection
