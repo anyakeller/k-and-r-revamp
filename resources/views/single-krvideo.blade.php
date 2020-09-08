db80e5ba2e0bae10e6be287293a480d96f1bc365
Anya L Keller <kelleranyal@gmail.com>
2020-09-08 17:48:48 -0400

    added div and margin for swiper thing

--- resources/views/single-krvideo.blade.php
+++ resources/views/single-krvideo.blade.php
@@ -24,23 +24,26 @@
 $all_videos = get_krvideos();
 @endphp
 {{-- swiper tests --}}
-<div class="swiper-container">
-  <!-- Additional required wrapper -->
-  <div class="swiper-wrapper">
-    @while($all_videos->have_posts())
-      @php $all_videos-> the_post()
-      @endphp
-      <div class="swiper-slide">
-        <img class="img-fluid" src="{{ the_field('wistia_related_video_thumbnail')  }}">
-        {{-- <div class="swiper-lazy-preloader"></div> --}}
-        <h6 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h6>
-      </div>
-      {{-- @include('partials.content-'.get_post_type()) --}}
-      @endwhile
-  </div>
+<div class="mt-4">
+  <div class="swiper-container">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+      @while($all_videos->have_posts())
+        @php $all_videos-> the_post()
+        @endphp
+        <div class="swiper-slide">
+          <img class="img-fluid" src="{{ the_field('wistia_related_video_thumbnail')  }}">
+          {{-- <div class="swiper-lazy-preloader"></div> --}}
+          <h6 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h6>
+        </div>
+        {{-- @include('partials.content-'.get_post_type()) --}}
+        @endwhile
+    </div>
 
-  <!-- If we need navigation buttons -->
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
+
+    <!-- If we need navigation buttons -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  </div>
 </div>
 @endsection
