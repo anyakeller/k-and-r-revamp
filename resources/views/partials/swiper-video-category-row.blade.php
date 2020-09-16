7588896cae289cd04a6a998f762661db5da90734
Anya L Keller <kelleranyal@gmail.com>
2020-09-16 15:31:21 -0400

    got video categories and icons to show up

--- resources/views/partials/swiper-video-category-row.blade.php
+++ resources/views/partials/swiper-video-category-row.blade.php
@@ -1,7 +1,13 @@
 <div class="col mt-4">
   <div class="swiper-container">
-    <!-- Additional required wrapper -->
+    {{-- Additional required wrapper  --}}
     <div class="swiper-wrapper">
+      {{-- The video category icon --}}
+      <div class="swiper-slide">
+        <img src="{{ the_field('video_category_icon_main',$category )  }}" alt="triangle with all three sides equal" height="87" width="100" />
+        <p>{{$category -> name}}</p>
+      </div>
+      {{-- The videos in the category --}}
       @while($one_category->have_posts())
         @php $one_category-> the_post()
         @endphp
@@ -12,7 +18,6 @@
             <p class="entry-title video-slide-text">{!! get_the_title() !!}</p>
           </a>
         </div>
-
         @endwhile
     </div>
     <!-- If we need navigation buttons -->
