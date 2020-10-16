09ce4e1cce8f338e1542db4473a6c0bb06e99546
Anya L Keller <kelleranyal@gmail.com>
2020-10-16 14:03:29 -0400

    redid some shitty class names

--- resources/views/components/video-card.blade.php
+++ resources/views/components/video-card.blade.php
@@ -2,14 +2,14 @@
   <a href="{{ get_permalink() }}">
     {{-- <img class="img-fluid" src="{{ the_field('wistia_related_video_thumbnail')  }}"> --}}
     <div class="video-thumbnail-div">
-      {{the_post_thumbnail('video-thumbnail', array('class' => 'swiper-slide-video-thumbnail' ))}}
+      {{the_post_thumbnail('video-thumbnail', array('class' => 'video-card-thumbnail' ))}}
       <div class="video-thumbnail-length-div">
         <span class="video-thumbnail-length-span">
           {!! App\get_video_length() !!}
         </span>
       </div>
     </div>
-    <p class="video-slide-text">
+    <p class="video-card-text">
       <span class="entry-title">{!! get_the_title() !!}</span>
       <span class="text-muted">{{App\time_elapsed_string(get_the_date())}}</span>
     </p>
