4796f5171c92409ec9724991d97adbb968776282
Anya L Keller <kelleranyal@gmail.com>
2020-10-16 14:01:31 -0400

    smd im amazing

--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,23 +1,3 @@
 <article @php post_class() @endphp>
-  <div class="video-card">
-    <a href="{{ get_permalink() }}">
-      <div class="video-thumbnail-div my-2">
-        {{the_post_thumbnail('video-thumbnail', array('class' => 'swiper-slide-video-thumbnail' ))}}
-        <div class="video-thumbnail-length-div">
-          <span class="video-thumbnail-length-span">
-            {!! App\get_video_length() !!}
-          </span>
-        </div>
-      </div>
-      <header class="py-1">
-        <p class="entry-title text-center">{!! get_the_title() !!}</p>
-        @if (get_post_type() === 'post')
-        @include('partials/entry-meta')
-        @endif
-      </header>
-    </a>
-  </div>
-  {{-- <div class="entry-summary">
-    @php the_excerpt() @endphp
-  </div> --}}
-  </article>
+  @include('components/video-card')
+</article>
