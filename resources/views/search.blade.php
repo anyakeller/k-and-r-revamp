35fbedd380fa86577a531342c10e89f31a79b906
Anya L Keller <kelleranyal@gmail.com>
2020-10-22 13:53:34 -0400

    fixed search on search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,23 +2,25 @@
 
 @section('content')
 @include('partials.page-header')
-
 @if (!have_posts())
-<div class="alert alert-warning">
+<div class="alert alert-warning mb-4">
   {{ __('Sorry, no results were found.', 'sage') }}
 </div>
 @endif
 
+{!! get_search_form(false) !!}
+
+
 <div class="container my-3">
   <div class="row row-cols-2 row-cols-md-3">
-  @while(have_posts())
+    @while(have_posts())
     <div class="col my-2">
       @php the_post()
       @endphp
       @include('partials.content-search')
     </div>
-  @endwhile
-</div>
+    @endwhile
+  </div>
 </div>
 
 {!! get_the_posts_navigation() !!}
