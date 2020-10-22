52e23257b736804d718562177bd8c6636ce440d1
Anya L Keller <kelleranyal@gmail.com>
2020-10-22 14:01:30 -0400

    styled labcam page

--- resources/views/page-labcam.blade.php
+++ resources/views/page-labcam.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.static')
 
 @section('content')
-  @include('partials.page-header')
-    <h1>UNDER CONSTRUCTION</h1>
-    <h3>stay tuned!</h3>
+@include('partials.page-header')
+<div class="text-center">
+  <i class="fas fa-hard-hat display-3"></i>
+  <h1 class="display-2 text-dark">UNDER CONSTRUCTION</h1>
+  <h3 class="display-4 text-muted">Stay tuned!</h3>
   {{-- @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile --}}
+      @include('partials.page-header')
+      @include('partials.content-page')
+    @endwhile --}}
+</div>
 @endsection
