ff66344846caa0e8d7756c8a6594c52fcf942656
Anya L Keller <kelleranyal@gmail.com>
2020-10-26 18:10:51 -0400

    the title of this branch is no longer accurate and the swiper slides are all messed up

--- resources/views/page-labcam.blade.php
+++ resources/views/page-labcam.blade.php
@@ -6,6 +6,7 @@
   <i class="fas fa-hard-hat display-3"></i>
   <h1 class="display-2 text-dark">UNDER CONSTRUCTION</h1>
   <h3 class="display-4 text-muted">Stay tuned!</h3>
+  @include('partials.content-page')
   {{-- @while(have_posts()) @php the_post() @endphp
       @include('partials.page-header')
       @include('partials.content-page')
--- resources/views/partials/content-single-krvideo.blade.php
+++ resources/views/partials/content-single-krvideo.blade.php
@@ -1,16 +1,17 @@
-<article @php post_class() @endphp>
+<article @php post_class('main-video mx-lg-auto') @endphp>
 
-  <div class="main-video mx-auto mb-4">
+  <div class="mx-auto mb-4">
 
     @include('components/main-video')
 
     {{-- <p>{{ the_field('video_details_video_length') }}</p> --}}
 
-    @php the_content() @endphp
+    @php the_content()
+    @endphp
   </div>
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav">
       <p>' . __('Pages:', 'sage'), 'after' => '</p>
     </nav>']) !!}
   </footer>
-</article>
+  </article>
