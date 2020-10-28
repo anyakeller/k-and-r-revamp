d9a8bb1648992460352898808b5229b6f123a0b0
Anya L Keller <kelleranyal@gmail.com>
2020-10-28 16:59:04 -0400

    redid swiper row css, must put in scss

--- resources/views/components/swiper-video-category-row.blade.php
+++ resources/views/components/swiper-video-category-row.blade.php
@@ -1,12 +1,12 @@
-<div class="swiper-slide-category">
+<div class="swiper-slide-category" style="position: absolute">
   <div class="category-icon-div"><img class="category-svg-icon" src="{{ the_field('video_category_icon_main',$category )  }}" alt="triangle with all three sides equal" /></div>
   <span>{{$category -> name}}</span>
   <div class="category-icon-offsetter"></div>
 </div>
-<div class="swiper-video-category-row-inner shift-left px-0">
-  <div class="swiper-container">
+{{-- <div class="swiper-video-category-row-inner px-0"> --}}
+  <div class="swiper-container" style="position: static;">
     {{-- Additional required wrapper  --}}
-    <div class="swiper-wrapper">
+    <div class="swiper-wrapper" style="height: auto;">
       {{-- The video category icon --}}
       {{-- <div class="swiper-slide swiper-slide-category">
         <div class="category-icon-div"><img class="category-svg-icon" src="{{ the_field('video_category_icon_main',$category )  }}" alt="triangle with all three sides equal" /></div>
@@ -26,4 +26,4 @@
     <div class="swiper-button-prev"></div>
     <div class="swiper-button-next"></div>
   </div>
-</div>
+{{-- </div> --}}
--- resources/views/single-krvideo.blade.php
+++ resources/views/single-krvideo.blade.php
@@ -12,17 +12,24 @@
 @endphp
 @include('partials.content-single-'.get_post_type())
 @endwhile
-
-@php $main_post_id = get_the_ID() @endphp
-@foreach( $video_categories as $category )
-<div class="swiper-video-category-row pt-3 mx-0">
-  @php $one_category = App\get_videos_from_category($category)
+<ul style="list-style: none;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+    margin-bottom: 25px;">
+  @php $main_post_id = get_the_ID()
   @endphp
-  @if($one_category->have_posts())
-    @include('components/swiper-video-category-row')
-    @endif
-</div>
-<hr />
-@endforeach
-
+  @foreach( $video_categories as $category )
+  <li style="position: relative">
+    <div class="swiper-video-category-row pt-3 mx-0">
+      @php $one_category = App\get_videos_from_category($category)
+      @endphp
+      @if($one_category->have_posts())
+        @include('components/swiper-video-category-row')
+        @endif
+    </div>
+    <hr />
+  </li>
+  @endforeach
+</ul>
 @endsection
