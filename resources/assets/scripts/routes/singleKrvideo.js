1bf3cb63ac7a2e0951c0f74c5cb98db8d74d16e2
Anya L Keller <kelleranyal@gmail.com>
2020-11-05 14:38:02 -0500

    hnork

--- resources/assets/scripts/routes/singleKrvideo.js
+++ resources/assets/scripts/routes/singleKrvideo.js
@@ -2,6 +2,10 @@ import Swiper from 'swiper/bundle';
 export default {
   init() {
     // JavaScript to be fired on a post page
+  },
+  finalize() {
+    // JavaScript to be fired on a post page, after the init JS
+
     new Swiper('.swiper-container', {
       slidesPerView: 3,
       // slidesPerView: 'auto',
@@ -38,8 +42,5 @@ export default {
         forceToAxis: true,
       },
     });
-  },
-  finalize() {
-    // JavaScript to be fired on a post page, after the init JS
   },
 };
