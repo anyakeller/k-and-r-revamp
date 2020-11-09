269c4b7462d73611571cfd44598ab8a571311337
Anya L Keller <kelleranyal@gmail.com>
2020-11-09 18:51:22 -0500

    button border radius styling

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,24 +9,10 @@
     </button>
     <div class="collapse navbar-collapse my-2 sidebar" id="navToggle">
       {{-- <div class=" sidebar-mobile"> --}}
-        @include('partials.sidebar')
+      @include('partials.sidebar')
       {{-- </div> --}}
     </div>
 
   </nav>
-  {{-- <div class="banner-row">
-    <div>
-      <div id="sticky-logo">
-        <a class="brand" href="{{ home_url('/') }}"><img id="kite-rocket-logo-img" src="@asset('images/kite-and-rocket-research-logo.png')" alt="{{ get_bloginfo('name', 'display') }}"></a>
-  </div>
-  <button class="hamburger hamburger--minus" type="button">
-    <span class="hamburger-box">
-      <span class="hamburger-inner"></span>
-    </span>
-  </button>
-  </div>
-  </div>
-  <div class="sidebar" id="sidebar-mobile">
-    @include('partials.sidebar')
-  </div> --}}
+
 </header>
