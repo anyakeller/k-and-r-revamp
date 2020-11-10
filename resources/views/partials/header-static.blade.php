b0313f11562bab82124035975f7fd3bef4bb1b9a
Anya L Keller <kelleranyal@gmail.com>
2020-11-10 14:02:28 -0500

    crunchy beans. Organized more css

--- resources/views/partials/header-static.blade.php
+++ resources/views/partials/header-static.blade.php
@@ -1,6 +1,6 @@
 <header class="banner container-main">
 
-  <nav class="navbar navbar-expand-sm">
+  <nav class="navbar navbar-expand-sm navbar-static">
     <a class="navbar-brand brand mr-sm-0" href="{{ home_url('/') }}"><img id="kite-rocket-logo-img" src="@asset('images/kite-and-rocket-research-logo.png')" alt="{{ get_bloginfo('name', 'display') }}"></a>
 
     <button class="navbar-toggler hamburger hamburger--minus" type="button" data-toggle="collapse" data-target="#navToggle" aria-controls="navToggle" aria-expanded="true" aria-label="Toggle navigation">
@@ -13,11 +13,11 @@
     </div>
 
   </nav>
-  @if (App\display_sidebar_static())
+  {{-- @if (App\display_sidebar_static())
   <div class="sidebar" id="sidebar-static">
     <nav class="navbar">
     @include('partials.sidebar-static')
     </nav>
   </div>
-  @endif
+  @endif --}}
 </header>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,12 @@
 <header class="banner container-main">
-  <nav class="navbar navbar-expand-sm">
+  <nav class="navbar navbar-mobile-only">
     <a class="navbar-brand brand mr-sm-0" href="{{ home_url('/') }}"><img id="kite-rocket-logo-img" src="@asset('images/kite-and-rocket-research-logo.png')" alt="{{ get_bloginfo('name', 'display') }}"></a>
     <button class="navbar-toggler hamburger hamburger--minus" type="button" data-toggle="collapse" data-target="#navToggle" aria-controls="navToggle" aria-expanded="true" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon hamburger-box">
         <span class="hamburger-inner"></span>
       </span>
     </button>
-    <div class="collapse navbar-collapse my-2 sidebar" id="navToggle">
+    <div class="collapse navbar-collapse my-2 sidebar sidebar-mobile-only" id="navToggle">
       {{-- <div class=" sidebar-mobile"> --}}
       @include('partials.sidebar')
       {{-- </div> --}}
