ea4f913363b3983db14a11ac57ac8d6558a40159
Anya L Keller <kelleranyal@gmail.com>
2020-11-16 18:45:00 -0500

    clean navs

--- resources/views/partials/header-static.blade.php
+++ resources/views/partials/header-static.blade.php
@@ -1,5 +1,5 @@
 <header class="banner container-main">
-  <nav class="navbar navbar-expand-sm navbar-static">
+  <nav class="navbar navbar-static">
     <a class="navbar-brand brand mr-sm-0" href="{{ home_url('/') }}"><img id="kite-rocket-logo-img" src="@asset('images/kite-and-rocket-research-logo.png')" alt="{{ get_bloginfo('name', 'display') }}"></a>
     <button class="navbar-toggler hamburger hamburger--minus" type="button" data-toggle="collapse" data-target="#navToggle" aria-controls="navToggle" aria-expanded="true" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon hamburger-box">
@@ -10,6 +10,6 @@
     <div class="collapse navbar-collapse mt-3 mb-0 sidebar" id="navToggle">
       @include('partials.sidebar')
     </div>
-    @endif  
+    @endif
   </nav>
 </header>
