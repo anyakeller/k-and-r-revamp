a6b1e82c8f2bbb0468b4fc02530e07236f48e779
Anya L Keller <kelleranyal@gmail.com>
2020-12-02 16:53:18 -0500

    adsfasdfds

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -13,9 +13,9 @@
 </div>
 
 <div class="video-grid container my-3">
-  <div class="row row-cols-2 row-cols-md-3">
+  <div class="row row-cols-3 ">
     @while(have_posts())
-    <div class="col my-2">
+    <div class="col my-2 px-2">
       @php the_post()
       @endphp
       @include('partials.content-search')
