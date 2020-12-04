5c36a05515ba7af59cd2689f4d0dea34701eb954
Anya L Keller <kelleranyal@gmail.com>
2020-12-04 14:38:13 -0500

    yeet

---
+++ app/Controllers/Partials/Categories.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers\Partials;
+
+trait Categories
+{
+  /**
+   * helper function to get all video categories
+   * uses get_terms()  https://developer.wordpress.org/reference/functions/get_terms/
+   */
+  public function videoCategories()
+  {
+    $args = array(
+      'taxonomy' => 'krvideo-cat',
+      'hide_empty' => true
+    );
+    return get_terms($args);
+  }
+}
--- app/Controllers/SingleKrvideo.php
+++ app/Controllers/SingleKrvideo.php
@@ -10,12 +10,5 @@ class SingleKrvideo extends Controller
    * helper function to get all video categories
    * uses get_terms()  https://developer.wordpress.org/reference/functions/get_terms/
    */
-  public function videoCategories()
-  {
-      $args = array(
-      'taxonomy' => 'krvideo-cat',
-      'hide_empty' => true
-    );
-      return get_terms($args);
-  }
+  use Partials\Categories;
 }
