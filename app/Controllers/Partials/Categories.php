c6c130a29825d9212e5766da765b4e3f85ff34c4
Anya L Keller <kelleranyal@gmail.com>
2020-12-04 15:15:12 -0500

    woao

--- app/Controllers/Partials/Categories.php
+++ app/Controllers/Partials/Categories.php
@@ -4,16 +4,16 @@ namespace App\Controllers\Partials;
 
 trait Categories
 {
-  /**
-   * helper function to get all video categories
-   * uses get_terms()  https://developer.wordpress.org/reference/functions/get_terms/
-   */
-  public function videoCategories()
-  {
-    $args = array(
-      'taxonomy' => 'krvideo-cat',
-      'hide_empty' => true
-    );
-    return get_terms($args);
-  }
+    /**
+     * helper function to get all video categories
+     * uses get_terms()  https://developer.wordpress.org/reference/functions/get_terms/
+     */
+    public function videoCategories()
+    {
+        $args = array(
+          'taxonomy' => 'krvideo-cat',
+          'hide_empty' => true
+        );
+        return get_terms($args);
+    }
 }
