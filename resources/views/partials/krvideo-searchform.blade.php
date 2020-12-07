5e8ed2100ad11799a3aa6cfd056e1d36c22fedf3
Anya L Keller <kelleranyal@gmail.com>
2020-12-07 13:01:47 -0500

    buggies

--- resources/views/partials/krvideo-searchform.blade.php
+++ resources/views/partials/krvideo-searchform.blade.php
@@ -1,7 +1,7 @@
 <form class="search-form" role="search" action="/" method="get">
   <label for="search">
     <span class="screen-reader-text">Search for:</span>
-    <input type="hidden" value="krvideo" name="post_type" id="post_type" />
-    <input type="text" class="search-field" name="s" id="search" placeholder="Search …" value="{{ the_search_query() }}" />
+    <input type="hidden" value="krvideo" name="post_type"/>
+    <input type="text" class="search-field" name="s" placeholder="Search …" value="{{ the_search_query() }}" />
   </label>
 </form>
