756ba4f301f73aad079602e7cbed73a01b91a07e
Anya L Keller <kelleranyal@gmail.com>
2021-01-13 12:51:43 -0500

    fixed leetle bug

--- resources/views/components/swiper-video-category-row.blade.php
+++ resources/views/components/swiper-video-category-row.blade.php
@@ -1,6 +1,8 @@
-<div class="swiper-slide-category">
-  <img class="category-svg-icon" src="{{ the_field('video_category_icon_main',$category )  }}" alt="triangle with all three sides equal" />
-  <span>{{$category -> name}}</span>
+<div style="display: flex;">
+  <div class="swiper-slide-category">
+    <img class="category-svg-icon" src="{{ the_field('video_category_icon_main',$category )  }}" alt="triangle with all three sides equal" />
+    <span>{{$category -> name}}</span>
+  </div>
 </div>
 <section class="swiper-container">
 
