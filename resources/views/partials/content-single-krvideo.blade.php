7a3754cbf885883e65e257068a71061f748c722a
Anya L Keller <kelleranyal@gmail.com>
2021-01-13 18:06:04 -0500

    added the video card border radius back

--- resources/views/partials/content-single-krvideo.blade.php
+++ resources/views/partials/content-single-krvideo.blade.php
@@ -4,21 +4,17 @@
     {{ the_field('wistia_related_wistia_embed_code') }}
   </div>
 
-  <div class="main-video-details">
-    <div class="d-flex">
-      <div class="flex-grow-1">
-        <h3>
-          {!! get_the_title() !!}
-          <span style="display: block;">
-            {{App\time_elapsed_string(get_the_date())}}
-          </span>
-        </h3>
-      </div>
-      <div class="float-right video-sharing-button-div">
-        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#video-sharing-modal">Share <i class="fas fa-share-square fa-xs"></i></button>
-      </div>
+  <div class="main-video-details text-center text-sm-left">
+    <h3>
+      {!! get_the_title() !!}
+    </h3>
+    <p class="">{{ the_field('video_details_video_description') }}</p>
+    <span style="display: block;">
+      {{App\time_elapsed_string(get_the_date())}}
+    </span>
+    <div class="video-sharing-button-div" style="display: none;">
+      <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#video-sharing-modal">Share <i class="fas fa-share-square fa-xs"></i></button>
     </div>
-    <p class="text-center text-sm-left">{{ the_field('video_details_video_description') }}</p>
   </div>
 
   @include('components/video-sharing-modal')
