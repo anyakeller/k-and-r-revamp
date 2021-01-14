611f125821ae7020a317aa4e918657036cba0543
Anya L Keller <kelleranyal@gmail.com>
2021-01-14 15:41:41 -0500

    reintegrated share btn

--- resources/views/partials/content-single-krvideo.blade.php
+++ resources/views/partials/content-single-krvideo.blade.php
@@ -4,16 +4,21 @@
     {{ the_field('wistia_related_wistia_embed_code') }}
   </div>
 
-  <div class="main-video-details text-center text-sm-left">
-    <h3>
-      {!! get_the_title() !!}
-    </h3>
+  <div class="main-video-details text-center text-md-left">
+    <div class="d-md-flex">
+      <h3 class="flex-md-grow-1">
+        {!! get_the_title() !!}
+      </h3>
+      <div class="d-none d-md-block">
+        <button type="button" class="btn btn-outline-primary share-btn" data-toggle="modal" data-target="#video-sharing-modal">Share <i class="fas fa-share-square fa-xs"></i></button>
+      </div>
+    </div>
     <p class="">{{ the_field('video_details_video_description') }}</p>
-    <span style="display: block;">
-      {{App\time_elapsed_string(get_the_date())}}
-    </span>
-    <div class="video-sharing-button-div" style="display: none;">
-      <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#video-sharing-modal">Share <i class="fas fa-share-square fa-xs"></i></button>
+    <div>
+      <span>
+        {{App\time_elapsed_string(get_the_date())}}
+      </span>
+      <button type="button" class="d-inline-block d-md-none btn btn-outline-primary ml-2 share-btn" data-toggle="modal" data-target="#video-sharing-modal">Share <i class="fas fa-share-square fa-xs"></i></button>
     </div>
   </div>
 
