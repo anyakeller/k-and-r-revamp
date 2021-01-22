520d87934bccfbab6326859c9ae3cba633e36b25
Anya L Keller <kelleranyal@gmail.com>
2021-01-22 12:25:46 -0500

    updated bootstrap

--- resources/views/page-labcam.blade.php
+++ resources/views/page-labcam.blade.php
@@ -1,17 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-  @include('partials.page-header')
-  <div class="text-center">
-    <i class="fas fa-hard-hat display-3"></i>
+  <div class="text-center mb-5">
+    <button class="btn btn-primary">I'm a button</button>
+    {{-- <i class="fas fa-hard-hat display-3"></i>
     <h1 class="display-2 text-dark">UNDER CONSTRUCTION</h1>
-    <h3 class="display-4 text-muted">Stay tuned!</h3>
-    @include('partials.content-page')
-    {{-- @while(have_posts()) @php the_post() @endphp
+    <h3 class="display-4 text-muted">Stay tuned!</h3> --}}
+    @while(have_posts()) @php the_post() @endphp
       @include('partials.page-header')
       @include('partials.content-page')
-    @endwhile --}}
+    @endwhile
   </div>
-</div>
 @endsection
